aaa30fdec728a66e85a11ec1f1caa0eea53e3253
asfdfjhbdkdsjhkfkhsjgdfkjhf <45268803+asfdfjhbdkdsjhkfkhsjgdfkjhf@users.noreply.github.com>
2018-12-23 14:49:33 +0200

    Add files via upload

--- bot.js
+++ bot.js
@@ -422,7 +422,7 @@ message.author.send(`**اوامر البوت :
 -----------------------------------------
 !inv : دعوة البوت لسيرفرك 
 !bot : معلومات البوت 
-سيرفر الدعم : https://discord.gg/asXKGw **`)
+سيرفر الدعم : https://discord.gg/YyGf47X **`)
 			  }
 			  
 			  });
@@ -491,6 +491,27 @@ client.on('guildCreate', guild => {
 
 
 
-
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'warda');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SERVER NAME **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+    
 
   client.login(process.env.TOKEN);
